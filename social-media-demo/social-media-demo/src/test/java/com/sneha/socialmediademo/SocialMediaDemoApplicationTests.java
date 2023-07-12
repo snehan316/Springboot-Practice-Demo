18a3fb6d6d483a882be9ef3052620d13c8a51e60
SNEHA N <sn263@dxc.com>
2023-07-12 14:35:26 +0530

    Initial Project Setup

---
+++ social-media-demo/social-media-demo/src/test/java/com/sneha/socialmediademo/SocialMediaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.socialmediademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
