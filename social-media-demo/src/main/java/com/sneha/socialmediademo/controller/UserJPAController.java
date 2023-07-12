2d5039bd4a6b7cd4168931a5d57aaa0a63ba1e79
SNEHA N <sn263@dxc.com>
2023-07-13 00:25:32 +0530

    Added methods for creating and retrieving posts

--- social-media-demo/src/main/java/com/sneha/socialmediademo/controller/UserJPAController.java
+++ social-media-demo/src/main/java/com/sneha/socialmediademo/controller/UserJPAController.java
@@ -1,6 +1,8 @@
 package com.sneha.socialmediademo.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -17,9 +19,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.sneha.socialmediademo.dao.UserDAOService;
+import com.ctc.wstx.shaded.msv_core.util.Uri;
 import com.sneha.socialmediademo.exceptions.UserNotFoundException;
+import com.sneha.socialmediademo.model.Post;
 import com.sneha.socialmediademo.model.User;
+import com.sneha.socialmediademo.repository.PostRepository;
 import com.sneha.socialmediademo.repository.UserRepository;
 
 import jakarta.validation.Valid;
@@ -28,25 +32,27 @@ import jakarta.validation.Valid;
 public class UserJPAController {
 
 	@Autowired
-	UserRepository repository;
+	UserRepository userRepository;
 	
+	@Autowired
+	PostRepository postRepository;
 	
 	
 	@PostMapping("/jpa/users")
 	public ResponseEntity<User> saveUser(@Valid @RequestBody User user) {
-	    User savedUser = repository.save(user);
+	    User savedUser = userRepository.save(user);
 	    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 		return ResponseEntity.created(location).build();
 	}
 	
 	@GetMapping("/jpa/users")
 	public List<User> findAllUsers(){
-		return repository.findAll();
+		return userRepository.findAll();
 	}
 	
 	@GetMapping("/jpa/users/{id}")
 	public EntityModel<User> findUserByID(@PathVariable int id) {
-		Optional<User> user = repository.findById(id);
+		Optional<User> user = userRepository.findById(id);
 		if(user.isEmpty()) {
 			throw new UserNotFoundException("User with id:" + id + " not found");
 		}
@@ -61,7 +67,37 @@ public class UserJPAController {
 	
 	@DeleteMapping("/jpa/users/{id}")
 	public void deleteUser(@PathVariable int id) {
-		 repository.deleteById(id);
+		 userRepository.deleteById(id);
 
 	}
+	
+	@GetMapping("/jpa/users/{id}/posts")
+	public List<Post> retrievePostsForUser(@PathVariable int id) {
+		 Optional<User> user = userRepository.findById(id);
+		 
+		 if(user.isEmpty()) {
+			 throw new UserNotFoundException("User with id: " + id + " not found");
+		 }
+		 
+		return user.get().getPosts();
+
+	}
+	
+	@PostMapping("jpa/users/{id}/posts")
+	public ResponseEntity<Object> createPostForUser(@PathVariable int id, @Valid @RequestBody Post post) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		if(user.isEmpty()) {
+			throw new UserNotFoundException("User with id: " + id + " not found");
+		}
+		post.setUser(user.get());
+		Post savedPost = postRepository.save(post);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedPost.getId()).toUri();
+				
+		return ResponseEntity.created(location).build();
+	}
+	
+	
 }
